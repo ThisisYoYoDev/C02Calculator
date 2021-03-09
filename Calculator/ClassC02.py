3b2516f5024766f09f181bac4152dc306bc04d1a
ThisisYoYoDev <71037962+ThisisYoYoDev@users.noreply.github.com>
2021-03-09 15:24:54 +0100

    [+] finish calculator v1

--- Calculator/ClassC02.py
+++ Calculator/ClassC02.py
@@ -1,5 +1,6 @@
-
 from Calculator.Const import *
+import pandas as pd
+
 
 class C02_init_world_for_server():
     VideoLOWConsumption = InternetLOWConsumption * LevelEnergyIntensityLOW # GB.h^-1
@@ -12,7 +13,82 @@ class C02_init_world_for_client():
     @staticmethod
     def Screen_Plasma(env):
         return Client(20 + env * 203, 5096 * env, 849 * env)
-    
+
     @staticmethod
     def ScreenLedLCD(env):
         return Client(20 + env * 172, 3218 * env, 536 * env)
+    PortableComputer = Client(40, 1362, 227)
+
+    PersonalComputer = Client(150, 2100, 350)
+
+    Projector = Client(135, 384, 64)
+
+    LowCodec = Client(26, 364, 61)
+
+    HighCodec = Client(80, 1120, 187)
+
+    Camera = Client(9.5, 120, 20)
+
+    Speaker = Client(4.1, 374, 62)
+
+    Micro = Client(2.5, 187, 31)
+
+    Router = Client(20, 1000, 167)
+
+
+class totalConsumption():
+    @staticmethod
+    def totalEngine(data):
+        ClientProP = C02_init_world_for_client()
+        evn = 0.24
+        lifetime = {
+            'laptop': 35040, 
+            'desktop' : 35040, 
+            'LED screen': 100000, 
+            'Plasma screen': 45000, 
+            'High performance codec': 87600, 
+            'Low performance codec': 87600, 
+            'Camera': 70080, 
+            'Speaker' : 17520, 
+            'Microphone' : 175200, 
+            'Router' : 30660
+        }
+        TotalEnergyUse = 0
+        hours = data['Hours']
+        operating_seconds = hours * 360
+
+        TotalEnergyUse += data['Number of laptops'] * ClientProP.PortableComputer.Power * operating_seconds
+        TotalEnergyUse += data['Number of laptops'] * ClientProP.PortableComputer.EnergyMan * hours/lifetime['laptop']
+
+        TotalEnergyUse += data['Number of desktops'] * ClientProP.PersonalComputer.Power * operating_seconds
+        TotalEnergyUse += data['Number of desktops'] * ClientProP.PersonalComputer.EnergyMan * hours/lifetime['desktop']
+
+        TotalEnergyUse += data['Number of desktops'] * (data['Percentage LED and LCD']/100) * ClientProP.ScreenLedLCD(evn).Power
+        TotalEnergyUse += data['Number of desktops'] * (data['Percentage LED and LCD']/100) * ClientProP.ScreenLedLCD(evn).EnergyMan * hours/lifetime['LED screen']
+
+        TotalEnergyUse += data['Number of desktops'] * ((100 - data['Percentage LED and LCD'])/100) * ClientProP.Screen_Plasma(evn).Power
+        TotalEnergyUse += data['Number of desktops'] * ((100 - data['Percentage LED and LCD'])/100) * ClientProP.Screen_Plasma(evn).EnergyMan * hours/lifetime['Plasma screen']
+
+        TotalEnergyUse += data['Number of extra LCD/LED screens'] * ClientProP.ScreenLedLCD(data['Area of extra LED/LCD screens']).Power
+        TotalEnergyUse += data['Number of extra LCD/LED screens'] * ClientProP.ScreenLedLCD(data['Area of extra LED/LCD screens']).EnergyMan * hours/lifetime['LED screen']
+
+        TotalEnergyUse += data['Number of extra plasma screens'] * ClientProP.ScreenLedLCD(data['Area of extra LED/LCD screens']).Power
+        TotalEnergyUse += data['Number of extra plasma screens'] * ClientProP.ScreenLedLCD(data['Area of extra plasma screens']).EnergyMan * hours/lifetime['Plasma screen']
+
+        TotalEnergyUse += data['Number of high performance CODECs'] * ClientProP.HighCodec.Power * operating_seconds
+        TotalEnergyUse += data['Number of high performance CODECs'] * ClientProP.HighCodec.EnergyMan * hours/lifetime['High performance codec']
+
+        TotalEnergyUse += data['Number of low performance CODECs'] * ClientProP.LowCodec.Power * operating_seconds
+        TotalEnergyUse += data['Number of low performance CODECs'] * ClientProP.LowCodec.EnergyMan * hours/lifetime['Low performance codec']
+
+        TotalEnergyUse += data['Number of cameras'] * ClientProP.Camera.Power * operating_seconds
+        TotalEnergyUse += data['Number of speakers'] * ClientProP.Speaker.Power * operating_seconds
+        TotalEnergyUse += data['Number of microphones'] * ClientProP.Micro.Power * operating_seconds
+        TotalEnergyUse += data['Number of routers'] * ClientProP.Router.Power * operating_seconds
+
+        TotalEnergyUse += data['Number of cameras'] * ClientProP.Camera.EnergyMan * hours/lifetime['Camera']
+        TotalEnergyUse += data['Number of speakers'] * ClientProP.Speaker.EnergyMan * hours/lifetime['Speaker']
+        TotalEnergyUse += data['Number of microphones'] * ClientProP.Micro.EnergyMan * hours/lifetime['Microphone']
+        TotalEnergyUse += data['Number of routers'] * ClientProP.Router.EnergyMan * hours/lifetime['Router']
+
+        return TotalEnergyUse
--- Calculator/main.py
+++ Calculator/main.py
@@ -1,3 +1,99 @@
+from Calculator.ClassC02 import totalConsumption
+import pandas as pd
 
 def main():
-    print("OK")
+    print("Saisissez le nombre d'heures pendant lesquelles la visioconférence aura lieu: ")
+    hours = int(input())
+
+    print('Combien de PC portable seront utilisés dans la visioconférence ?')
+    num_pcs = int(input())
+    
+    print('Quel pourcentage approximatif d’entre eux sont des ordinateurs de bureau ? (Entrez u pour inconnu)')
+    percent_desktop = input()
+    
+    if percent_desktop == 'u':
+        percent_desktop=57.1
+    
+    num_desktops = num_pcs * int(percent_desktop) / 100
+    num_laptops = num_pcs - num_desktops
+    
+    print('Quel pourcentage de ces ordinateurs de bureau utilisent des écrans LED ou LCD (entrez u pour inconnu)')
+    
+    per_LED = input()
+    if (per_LED == 'u'):
+        per_LED = 78.6
+    
+    print('Combien d\'écrans LCD / LED supplémentaires sont utilisés ?')
+    extra_led = int(input())
+    
+    print('Quelle est la taille de ces écrans (pouces)? Entrez u pour inconnu')
+    width_led = input()
+    
+    if width_led == 'u':
+        width_led = 60
+    width_led = int(width_led)
+
+    height_led = (width_led / 16) * 9
+    area_led = width_led * height_led
+    area_led *= 0.00064516
+    
+    print('Combien d\'écrans plasma supplémentaires sont utilisés ?')
+    extra_plasma = int(input())
+    
+    print('Quelle est la taille de ces écrans (pouces)? Entrez u pour inconnu')
+    width_plasma = input()
+    
+    if width_plasma == 'u':
+        width_plasma = 60
+    width_plasma = int(width_plasma)
+    
+    height_plasma = (width_plasma/16) * 9
+    area_plasma = width_plasma * height_plasma
+    area_plasma *= 0.00064516
+    
+    print('Nombre de CODEC haut de gamme utilisés :')
+    num_high_codecs = int(input())
+    
+    print('Nombre de CODEC bas de gamme utilisés :')
+    num_low_codecs = int(input())
+    
+    print('Nombre de caméras utilisées :')
+    num_cameras = int(input())
+    
+    print('Nombre de haut-parleurs utilisés :')
+    num_speakers = int(input())
+    
+    print('Nombre de microphones utilisés :')
+    num_microphones = int(input())
+    
+    print('Nombre de routeurs utilisés :')
+    num_routers = int(input())
+    data = {
+        'Hours': hours, 
+        'Number of desktops': num_desktops, 
+        'Number of laptops': num_laptops, 
+        'Percentage LED and LCD': int(per_LED), 
+        'Number of extra LCD/LED screens': extra_led, 
+        'Area of extra LED/LCD screens': area_led, 
+        'Number of extra plasma screens': extra_plasma, 
+        'Area of extra plasma screens': area_plasma, 
+        'Number of high performance CODECs': num_high_codecs, 
+        'Number of low performance CODECs': num_low_codecs,
+        'Number of cameras': num_cameras,
+        'Number of speakers': num_speakers, 
+        'Number of microphones': num_microphones, 
+        'Number of routers': num_routers
+    }              
+    return data
+
+data = main()
+print(data)
+energy = totalConsumption.totalEngine(data)
+co2 = 0.6*(energy/3600000)
+print('The carbon dioxide produced by this videoconference is: {:.3f} in kgC02e'.format(co2))
+
+data["Consommation"] = str(co2)
+df = pd.DataFrame(data=data, index=[0])
+df.to_excel('test.xlsx')
+
+exit(0)
